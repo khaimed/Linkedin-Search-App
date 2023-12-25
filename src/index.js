0c79043de891a59c9614a94908260d3cde5ebc15
Khalid Ait M'hamed <khalid.aitmhamed@hotmail.com>
2023-12-25 15:06:52 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const { Builder, Key, By } = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+
+let options = new chrome.Options();
+options.addArguments('--headless');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -12,8 +17,11 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'src/preload.js'),
+      contextIsolation: false,
+      contentSecurityPolicy: "script-src 'self' 'unsafe-eval' 'zwxpBwzZbURUl3JTSABruEg1kvcUuFAJ' 'sha256-2l30QxSunNDaaNuCPRFcr2eKTYDRur0Sa2UznSlq8LQ='",
+    }
   });
 
   // and load the index.html of the app.
@@ -45,5 +53,72 @@ app.on('activate', () => {
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+// this is the beginning of code
+
+ipcMain.on('call-them', async (event, allThem) => {
+  let inputUser = allThem[0]
+  let inputPassword = allThem[1]
+  let inputSearch = allThem[2]
+  let selectValue = allThem[3]
+
+  try {
+    driver = await new Builder()
+    .forBrowser('chrome')
+    // .setChromeOptions(options)
+    .build();
+    await driver.manage().window().maximize();
+    // Link
+    let url = 'https://www.linkedin.com/';
+    await driver.get(url)
+
+    // login
+    await driver.findElement(By.xpath('/html/body/main/section[1]/div/div/form/div[1]/div[1]/div/div/input')).sendKeys(inputUser)
+    await driver.findElement(By.xpath('/html/body/main/section[1]/div/div/form/div[1]/div[2]/div/div/input')).sendKeys(inputPassword,Key.ENTER)
+
+    // search
+    switch (selectValue) {
+      case "tout":
+        await driver.get("https://www.linkedin.com/search/results/all/");
+        break;
+      case "personnes":
+        await driver.get("https://www.linkedin.com/search/results/people/");
+        break;
+      case "emplois":
+        await driver.get("https://www.linkedin.com/jobs/search/");
+        break;
+      case "entreprises":
+        await driver.get("https://www.linkedin.com/search/results/companies/");
+        break;
+      case "produits":
+        await driver.get("https://www.linkedin.com/search/results/products/");
+        break;
+      case "groupes":
+          await driver.get("https://www.linkedin.com/search/results/groups/");
+        break;
+      case "services":
+          await driver.get("https://www.linkedin.com/search/results/services/");
+        break;
+      case "evenements":
+          await driver.get("https://www.linkedin.com/search/results/events/");
+        break;
+      case "cours":
+          await driver.get("https://www.linkedin.com/search/results/learning/");
+        break;
+      case "école":
+          await driver.get("https://www.linkedin.com/search/results/schools/");
+        break;
+      default:
+        break;
+    }
+    if(selectValue !== "emplois"){
+      await driver.findElement(By.xpath('/html/body/div[4]/header/div/div/div/div[1]/input')).sendKeys(inputSearch,Key.ENTER)
+    } else {
+      await driver.findElement(By.css('[id^="jobs-search-box-keyword-id-"]')).sendKeys(inputSearch,Key.ENTER)
+    }
+
+    // await driver.quit();
+  } catch (error){
+    console.log("the error is :", error)
+    await driver.quit();
+  }
+})
--- src/preload.js
+++ src/preload.js
@@ -1,16 +1,12 @@
 const { ipcRenderer } = require("electron")
+const shell = require("electron").shell;
 
-let khaimedButton = document.getElementById("khaimed_button");
 let facebookButton = document.getElementById("facebook_button");
 let instagramButton = document.getElementById("instagram_button");
 let twitterButton = document.getElementById("twitter_button");
 let youtubeButton = document.getElementById("youtube_button");
 let linkedinButton = document.getElementById("linkedin_button");
 
-// click for my link
-khaimedButton.addEventListener("click", () => {
-  shell.openExternal("https://www.khaimed.com/");
-});
 // click for open browser
 facebookButton.addEventListener("click", () => {
   shell.openExternal("https://www.facebook.com/khaimedev");
@@ -28,41 +24,12 @@ youtubeButton.addEventListener("click", () => {
   shell.openExternal("https://www.youtube.com/@khaimedev");
 });
 
-document.querySelector(".search").addEventListener("click", () => {
-    const selectValue = document.querySelector("select").value;
-    const inputValue = document.querySelector("input[type=text]").value;
-    switch (selectValue) {
-        case "tout":
-            ipcRenderer.send("tout-caller", inputValue);
-        break;
-        case "personnes":
-            ipcRenderer.send("personnes-caller", inputValue);
-        break;
-        case "emplois":
-            ipcRenderer.send("emplois-caller", inputValue);
-        break;
-        case "entreprises":
-            ipcRenderer.send("entreprises-caller", inputValue);
-        break;
-        case "produits":
-            ipcRenderer.send("produits-caller", inputValue);
-        break;
-        case "groupes":
-            ipcRenderer.send("groupes-caller", inputValue);
-        break;
-        case "services":
-            ipcRenderer.send("services-caller", inputValue);
-        break;
-        case "evenements":
-            ipcRenderer.send("evenements-caller", inputValue);
-        break;
-        case "cours":
-            ipcRenderer.send("cours-caller", inputValue);
-        break;
-        case "ecoles":
-            ipcRenderer.send("ecoles-caller", inputValue);
-        break;
-        default:
-        break;
-    }
-  });
+document.getElementById('run').addEventListener("click", () => {
+  const inputUser = document.getElementsByName('username')[0].value
+  const inputPassword = document.getElementsByName('password')[0].value;
+  const inputSearch = document.getElementsByName('search')[0].value;
+  const selectValue = document.getElementsByName('choose')[0].value;
+  const allThem = [inputUser,inputPassword,inputSearch,selectValue];
+  ipcRenderer.send("call-them", allThem);
+});
+
